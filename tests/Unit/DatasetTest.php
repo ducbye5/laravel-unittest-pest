9e42af3ddf96d72a0c1e0436154892ea265b449b
do minh duc <ducbye5@gmail.com>
2024-07-07 20:44:09 +0700

    master

--- tests/Unit/DatasetTest.php
+++ tests/Unit/DatasetTest.php
@@ -22,7 +22,7 @@ describe('division', function () {
         $result = $this->controller->division($divisor, $dividend);
     
         expect($result)->toBe($expectResult);
-    })->with('divisionDatasetInfile');
+    })->with('datasets');
 
     // Sharing Datasets
     test('case correct data 3', function (int $divisor, int $dividend, $expectResult) {
@@ -50,10 +50,10 @@ describe('division', function () {
         $result = $this->controller->division($divisor, $dividend);
     
         expect($result)->toBe($expectResult);
-    })->with('divisionDatasetInfile')->repeat(10);
+    })->with('datasets')->repeat(10);
 })->group("DemoController_division");
 
-dataset('divisionDatasetInfile', [
+dataset('datasets', [
     'positive numbers' => [4, 2, 2],
     'negative numbers' => [-1, -2, 0.5],
     'using closure' => [fn () => 6, 2, 3]
